c5d9e439339aa638a368842bb6217143ae64c42e
Akhil Parihar <166745813+akhilparihar-dev@users.noreply.github.com>
2026-06-23 15:45:00 +0530

    Create TwoSumOptimal.java

---
+++ HashMap /TwoSumOptimal.java
@@ -0,0 +1,19 @@
+// Problem: Two Sum
+// LeetCode: #1
+// Approach: HashMap
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int num=nums[i];
+            int complement=target-num;
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+            map.put(num,i);
+        }
+        return new int[]{};
+    }
+}
